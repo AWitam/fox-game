47af77bef6487eba43ff4349dc87ea1532fd9af2
Ada <witamborska.ada@gmail.com>
2020-11-27 16:14:16 +0100

    display UI

---
+++ src/init.js
@@ -0,0 +1,21 @@
+import game from "./gameState";
+import { TICK_RATE } from "./constants";
+import initButtons from "./buttons";
+
+async function init() {
+  console.log("I'm starting rn");
+  initButtons(game.handleUserAction);
+  let nextTimeToTick = Date.now();
+
+  function nextAnimationFrame() {
+    const now = Date.now();
+
+    if (nextTimeToTick <= now) {
+      game.tick();
+      nextTimeToTick = now + TICK_RATE;
+    }
+    requestAnimationFrame(nextAnimationFrame);
+  }
+  requestAnimationFrame(nextAnimationFrame);
+}
+init();
